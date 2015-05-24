f571a8255e77333b766a4abd1dfa0a78239bdbca
KeKeMic <kirby830730@gmail.com>
2015-05-24 16:44:27 +0800

    add Button, Frame

---
+++ Calculator/src/tw/edu/npu/mis/ClickButton.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tw.edu.npu.mis;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author STP
+ */
+public class ClickButton extends JButton{
+    public ClickButton (String name){
+        
+    }
+}
---
+++ Calculator/src/tw/edu/npu/mis/MainFrame.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tw.edu.npu.mis;
+
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author STP
+ */
+public class MainFrame extends JFrame{
+    public MainFrame(String title){
+        this.setTitle(title);
+        initCimponents();
+    }
+
+    private void initCimponents() {
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        Container contentPane = this.getContentPane();
+        contentPane.setLayout(new GridBagLayout());
+        GridBagConstraints Gbc = new GridBagConstraints();
+        Gbc.insets = new Insets(2,2,2,2);
+        
+    }
+}
